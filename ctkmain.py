b171f0952a055e7b274516fc6c38d5235f162145
Lucca Oliveira <lucca0505@gmail.com>
2024-11-21 16:37:56 -0300

    criação de botões e textos para janela de inserção de tarefas

--- ctkmain.py
+++ ctkmain.py
@@ -46,4 +46,5 @@ butt_deleta.place(x=posicao_butts+450,y=250)
 # tv.insert("","end",values=["Dever de casa","17/10/2024","ALTA","Escola","Em andamento"])
 # tv.insert("","end",values=["Fazer o jantar","25/10/2024","MEDIA","Casa","Não iniciada"])
 
+# inicializa a janela
 win_main.mainloop()
--- functions.py
+++ functions.py
@@ -32,12 +32,50 @@ class Gerenciador():
             self.tem_janela_aberta = True
             win_insere = CTkToplevel()
             win_insere.title("Inserir nova tarefa")
+            # win_insere.resizable(False,False)
             centralizar_janela(win_insere,main_x,main_y)
-            win_insere.geometry("400x200")
+            win_insere.geometry("810x300")
             # comando para dar foco na janela Devido a bug do CTk
             win_insere.after(100, win_insere.lift)
             win_insere.protocol("WM_DELETE_WINDOW",lambda: self.gatilhar_fechamento(win_insere))
-
+            
+            # criar como inserir tarefa
+            label_intro = CTkLabel(win_insere,text="Por favor preencha todos os campos para cadastro da nova tarefa", font=("Arial",20))
+            label_intro.place(x=10,y=0)
+            
+            label_titulo = CTkLabel(win_insere,text="Título:",font=("Arial",15))
+            label_titulo.place(x=10,y=70)
+            box_entry_titulo = CTkTextbox(win_insere,width=150,height=10)
+            box_entry_titulo.place(x=10,y=100)
+            
+            label_data = CTkLabel(win_insere,text="Data:",font=("Arial",15))
+            label_data.place(x=170,y=70)
+            box_entry_data = CTkTextbox(win_insere,width=150,height=10)
+            box_entry_data.place(x=170,y=100)
+            
+            label_prioridade = CTkLabel(win_insere,text="Prioridade:",font=("Arial",15))
+            label_prioridade.place(x=330,y=70)
+            box_entry_data = CTkTextbox(win_insere,width=150,height=10)
+            box_entry_data.place(x=330,y=100)
+            
+            label_categoria = CTkLabel(win_insere,text="Categoria:",font=("Arial",15))
+            label_categoria.place(x=490,y=70)
+            box_entry_categoria = CTkTextbox(win_insere,width=150,height=10)
+            box_entry_categoria.place(x=490,y=100)
+            
+            label_status = CTkLabel(win_insere,text="Status:",font=("Arial",15))
+            label_status.place(x=650,y=70)
+            box_entry_status = CTkTextbox(win_insere,width=150,height=10)
+            box_entry_status.place(x=650,y=100)
+            
+            label_obs = CTkLabel(win_insere,text="Observações:",font=("Arial",15))
+            label_obs.place(x=10,y=150)
+            box_entry_obs = CTkTextbox(win_insere,width=790,height=110)
+            box_entry_obs.place(x=10,y=180)
+            
+            butt_confirm = criar_botao_ttk(win_insere,100,"Confirmar",20,lambda: print("Foda-se"))
+            butt_confirm.place(x=695,y=10)
+            
     def atualizar_tarefa(self) -> None:
         if self.tem_janela_aberta:
             pass
@@ -45,6 +83,7 @@ class Gerenciador():
             self.tem_janela_aberta = True
             win_atualiza = CTkToplevel()
             win_atualiza.title("Atualizar Tarefa")
+            # win_atualiza.resizable(False,False)
             centralizar_janela(win_atualiza,main_x,main_y)
             win_atualiza.geometry("600x300")
             # comando para dar foco na janela Devido a bug do CTk
@@ -58,6 +97,7 @@ class Gerenciador():
             self.tem_janela_aberta = True
             win_exibe = CTkToplevel()
             win_exibe.title("Exibir Tarefa")
+            # win_exibe.resizable(False,False)
             centralizar_janela(win_exibe,main_x,main_y)
             win_exibe.geometry("500x450")
             # comando para dar foco na janela Devido a bug do CTk
@@ -71,6 +111,7 @@ class Gerenciador():
             self.tem_janela_aberta = True
             win_deleta = CTkToplevel()
             win_deleta.title("Deletar Tarefa")
+            # win_deleta.resizable(False,False)
             centralizar_janela(win_deleta,main_x,main_y)
             win_deleta.geometry("400x200")
             # comando para dar foco na janela Devido a bug do CTk
